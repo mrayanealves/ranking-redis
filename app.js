e4ba2fb5c7e04d60b18c5655ea4fa2cbb076b11f
Rayane <mrayalves05@gmail.com>
2019-08-29 15:53:14 -0300

    Adicionando controller playing

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-var index = require('./index')
+var index = require('./controller/index')
+var playing = require('./controller/playing');
 
 app.listen(port, () => console.log(`Example app listening on port $
 {port}!`))
 
 app.use('/', index)
+
+app.use('/playing', playing)
