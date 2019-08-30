de901bb1ad38150b527fe1901136cd11e597bb54
Rayane <mrayalves05@gmail.com>
2019-08-30 11:33:42 -0300

    Update index

--- controller/index.js
+++ controller/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res) {
-    res.status(404).end();
+    res.status(200).jsonp({"message" : "Welcome to the Rayranking"});
 }); 
 
 module.exports = router
